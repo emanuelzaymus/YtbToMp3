8dad98fcdb39ac15a681e25fbe023c0650586277
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2021-10-03 10:58:41 +0200

    Working ToutubeToMp3

---
+++ YtbToMp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YtbToMp3
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2)
+            {
+                PrintHelp();
+                return -1;
+            }
+
+            string youtubeUrlsFile = args[0];
+            string outputDirectory = args.Length > 1 ? args[1] : ".";
+
+
+            Console.WriteLine("Started");
+            Stopwatch stopwatch = new();
+
+            stopwatch.Start();
+
+            using (CancellationTokenSource cancellationTokenSource = new())
+            {
+                var youtubrUrls = await File.ReadAllLinesAsync(youtubeUrlsFile, cancellationTokenSource.Token);
+
+                YoutubeToMp3 youtubeToMp3 = new();
+
+                await youtubeToMp3.DownloadAsync(youtubrUrls, outputDirectory, cancellationToken: cancellationTokenSource.Token);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("End");
+
+            return 0;
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine(
+                "YtbToMp3.exe has wrong number of input arguments. \n\n" +
+                "YtbToMp3.exe Arguments: \n" +
+                "    1. Text file with Youtube urls, e.g.: \n" +
+                "        https://www.youtube.com/watch?v=2UxUJR31MEe \n" +
+                "        https://www.youtube.com/watch?v=osfDwb9U95w \n" +
+                "        https://www.youtube.com/watch?v=LqFtyWFG85q \n" +
+                "        ... \n\n" +
+                "    2. Output directory where to save the files. (Optional - default: \".\") \n\n" +
+                "E.g.: YtbToMp3.exe song_urls.txt ./my_songs/"
+            );
+        }
+
+    }
+}
---
+++ YtbToMp3/YoutubeToMp3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos;
+
+namespace YtbToMp3
+{
+    internal class YoutubeToMp3
+    {
+        private const string Mp3Extension = ".mp3";
+
+        private readonly YoutubeClient Youtube = new();
+
+        public async Task DownloadAsync(IEnumerable<string> youtubeUrls, string saveToDirectory = null,
+            IProgress<double> progress = null, CancellationToken cancellationToken = default)
+        {
+            var allTasks = new List<Task>();
+
+            foreach (var url in youtubeUrls)
+            {
+                allTasks.Add(DownloadAsync(url, saveToDirectory, progress, cancellationToken));
+            }
+
+            await Task.WhenAll(allTasks);
+        }
+
+        private async Task DownloadAsync(string youtubeUrl, string saveToDirectory = null,
+            IProgress<double> progress = null, CancellationToken cancellationToken = default)
+        {
+            Video video = await GetVideoAsync(youtubeUrl);
+
+            string mp3FileName = CreateMp3FileName(video.Title);
+
+            string outputFilePath = CombineFilePath(saveToDirectory, mp3FileName);
+
+            await Youtube.Videos.DownloadAsync(youtubeUrl, outputFilePath, progress, cancellationToken);
+        }
+
+        private async Task<Video> GetVideoAsync(string youtubeUrl)
+        {
+            return await Youtube.Videos.GetAsync(youtubeUrl);
+        }
+
+        private string CreateMp3FileName(string videoTitle)
+        {
+            string fileName = videoTitle.Trim() + Mp3Extension;
+
+            return ReplaceAllInvalidFileNameChars(fileName, '_');
+        }
+
+        private string CombineFilePath(string saveToDirectory, string mp3FileName)
+        {
+            if (saveToDirectory is not null)
+            {
+                Directory.CreateDirectory(saveToDirectory);
+
+                return Path.Combine(saveToDirectory, mp3FileName);
+            }
+
+            return mp3FileName;
+        }
+
+        private string ReplaceAllInvalidFileNameChars(string fileName, char replaceWithChar)
+        {
+            foreach (var invalidChar in Path.GetInvalidFileNameChars())
+            {
+                fileName.Replace(invalidChar, replaceWithChar);
+            }
+
+            return fileName;
+        }
+
+    }
+}
